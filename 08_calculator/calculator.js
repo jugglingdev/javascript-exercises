2b0f5d9e7f67c628ac90cd243d2649336eec3186
jugglingdev <kaylamarie1785@gmail.com>
2023-04-15 18:09:05 -0500

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return parseInt(num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return parseInt(num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for (let item in array) {
+    sum += array[item];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let answer = 1;
+  for (let item in array) {
+    answer *= array[item];
+  }
+  return answer;
 };
 
-const power = function() {
-	
+const power = function(num, expo) {
+	return parseInt(num ** expo);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let answer = 1;
+  for (let i = 1; i <= num; i++) {
+    answer *= i;
+  }
+  return answer;
 };
 
 // Do not edit below this line
