72b644fb25dadb47754147f164ccd1705b398d94
jugglingdev <kaylamarie1785@gmail.com>
2023-04-15 20:20:11 -0500

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    
+    if (n < 0) return 'OOPS';
 
+    let num1 = 0;
+    let num2 = 1;
+    let prod = num1 + num2;
+
+    for (let i = 1; i < n - 1; i++) {
+        num1 = num2;
+        num2 = prod;
+        prod = num1 + num2;
+    }
+
+    return prod;
 };
 
 // Do not edit below this line
