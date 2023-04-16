eed22858d6417487590b5675de3cfce90e6b3244
jugglingdev <kaylamarie1785@gmail.com>
2023-04-15 19:37:54 -0500

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    
+    let inputLettersOnly = input.toLowerCase().replace(/[^a-z]/g, '');
+    let inputArray = [...inputLettersOnly];
 
+    return inputArray.toString() == inputArray.reverse().toString();
+ 
 };
 
 // Do not edit below this line
